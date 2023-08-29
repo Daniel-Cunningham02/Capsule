3423849f13177c564d74f4166f2fbcaf0a913035
Daniel-Cunningham02 <danrcunn@ut.utm.edu>
2023-08-29 12:04:13 -0500

    Added boilerplate and examples of new files

--- Server/main.go
+++ Server/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	router := Setup()
+}
+
+func Setup() *gin.Engine {
+	router := gin.Default()
+	router.GET("/lib?=:package", sendLib)
+	router.GET("/dll?=:package", sendDll)
+	router.GET("/src?=:package", sendSrc)
+	router.PUT("/packages?=:package", upload)
+	return router
 }
