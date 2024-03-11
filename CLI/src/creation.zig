fc7e837fc34803c36895a3f539d28b62b13851c0
Daniel-Cunningham02 <danrcunn@ut.utm.edu>
2024-03-11 16:09:16 -0500

    Can run python setup now

--- CLI/src/creation.zig
+++ CLI/src/creation.zig
@@ -1,17 +1,16 @@
 const std = @import("std");
 const messages = @import("messages.zig");
 const types = @import("types.zig");
-const process = std.ChildProcess;
 const mem = std.mem;
 
 const CapsuleObject = struct { name: []u8, file: []u8, dir: []u8, version: []u8, desc: []u8, dependencies: []u8 };
 
 pub fn initCapsuleFile() !void {
-    const result = try process.exec(.{
-        .allocator = std.heap.page_allocator,
-        .argv = &[_][]const u8{ "python3", "initialize.zip" },
-    });
-    try std.io.getStdOut().writer().print("{s}", .{result.stderr});
+    const result = std.process.execv(
+        std.heap.page_allocator,
+        &[_][]const u8{ "python3", "initialize.zip" },
+    );
+    try std.io.getStdOut().writer().print("{}", .{result});
 }
 
 pub fn newCapsuleFile(args: *const [][:0]u8) !void {
